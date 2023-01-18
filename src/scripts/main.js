23156be6cf751ffc94f03f517848bce6562d3027
Paulo César <pcgms7@hotmail.com>
2023-01-18 12:14:37 -0300

    finalizando projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    // Seção FAQ, acordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+}) 
+
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
